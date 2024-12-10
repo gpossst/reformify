08e4d1bc60fbad22dc1330f733d6b75b9a17fe49
gpostVT <gep100iosgames@gmail.com>
2024-12-09 22:40:47 -0500

    please work

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -8,6 +8,11 @@ const handler = NextAuth({
     GithubProvider({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
+      authorization: {
+        params: {
+          redirect_uri: `${process.env.NEXTAUTH_URL}/api/auth/callback/github`,
+        },
+      },
     }),
   ],
   adapter: MongoDBAdapter(clientPromise, {
@@ -16,7 +21,6 @@ const handler = NextAuth({
       Users: "users",
     },
   }),
-  // Add these configurations
   useSecureCookies: true,
   cookies: {
     sessionToken: {
@@ -27,11 +31,6 @@ const handler = NextAuth({
         path: "/",
         secure: true,
       },
-    },
-  },
-  callbacks: {
-    redirect({ url, baseUrl }) {
-      return url.startsWith(baseUrl) ? url : baseUrl;
     },
   },
 });
