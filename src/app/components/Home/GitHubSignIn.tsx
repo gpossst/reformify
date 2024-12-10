6f3329286fa96df25556b62826639dae16d34cbb
gpostVT <gep100iosgames@gmail.com>
2024-12-09 22:53:32 -0500

    revert

--- src/app/components/Home/GitHubSignIn.tsx
+++ src/app/components/Home/GitHubSignIn.tsx
@@ -40,10 +40,7 @@ export default function GitHubSignIn({ size }: { size: number }) {
         <button
           className="px-2 flex items-center gap-2 text-foreground text-lg font-semibold py-1 bg-accent border-accent border-2 rounded-md hover:bg-accent"
           onClick={() =>
-            signIn("github", {
-              callbackUrl: "/api/auth/callback/github",
-              redirect: true,
-            })
+            signIn("github", { callbackUrl: "/dashboard", redirect: true })
           }
         >
           <FaGithub className="text-foreground" size={24} />
