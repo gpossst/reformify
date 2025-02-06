fb276e579e0ad1322014d106754b59b1cd6ed7f0
gpostVT <gep100iosgames@gmail.com>
2025-02-06 12:42:03 -0500

    fix: ensuring distinct form key

--- src/app/api/apyhub/create_form/route.ts
+++ src/app/api/apyhub/create_form/route.ts
@@ -1,10 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { MongoClient, ObjectId } from "mongodb";
 
-export async function GET() {
-  return NextResponse.json({ message: "Hello World" });
-}
-
 export async function POST(request: NextRequest) {
   const userId = request.headers.get("x-customer-id");
 
@@ -29,6 +25,18 @@ export async function POST(request: NextRequest) {
 
     const forms = db.collection("forms");
     const formId = new ObjectId();
+
+    const isFormKeyUnique = await forms.findOne({
+      formKey,
+      userId,
+    });
+
+    if (isFormKeyUnique) {
+      return NextResponse.json(
+        { error: "Form key already exists" },
+        { status: 400 }
+      );
+    }
 
     const form = await forms.insertOne({
       _id: formId,
